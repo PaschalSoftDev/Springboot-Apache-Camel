41e352835fc57d9fdbbfb5b8f37533b1473622b9
Paschal E. C <paschalsoftdev@gmail.com>
2022-12-28 16:44:34 +0100

    Add files via upload

---
+++ boot-camel/src/test/java/com/CamelSpringExample/bootcamel/BootCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CamelSpringExample.bootcamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
